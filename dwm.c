27119443a182087496b8cc792e06e73910e0f122
root <root>
2024-12-08 17:58:39 +0800

    open the specified tag will execute the specified command

--- dwm.c
+++ dwm.c
@@ -82,7 +82,7 @@ enum { WMProtocols, WMDelete, WMState, WMTakeFocus, WMLast }; /* default atoms *
 enum { ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
        ClkClientWin, ClkRootWin, ClkLast }; /* clicks */
 
-typedef union {
+typedef struct {
 	int i;
 	unsigned int ui;
 	float f;
@@ -2925,6 +2925,8 @@ view(const Arg *arg)
 {
 	int i;
 	unsigned int tmptag;
+    int n=0;
+    Client *c;
 
     if ((arg->ui & TAGMASK) == selmon->tagset[selmon->seltags]) {
             if (arg->ui == ~0) {
@@ -2964,6 +2966,15 @@ view(const Arg *arg)
 
 	focus(NULL);
 	arrange(selmon);
+    // 若当前tag无窗口 且附加了v参数 则执行
+    if (arg->v) {
+        for (c = selmon->clients; c; c = c->next)
+            if (c->tags & arg->ui && !HIDDEN(c))
+                n++;
+        if (n == 0) {
+            spawn(&(Arg){ .v = (const char*[]){ "/bin/sh", "-c", arg->v, NULL } });
+        }
+    }
 }
 
 Client *
